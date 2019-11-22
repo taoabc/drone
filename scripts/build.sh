810b6810bb9e864b2b5b9b21024b1c6890efdf09
huhaitao <huhaitao@100tal.com>
2019-11-22 18:08:22 +0800

    remove agent build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,3 @@ REPO="github.com/drone/drone"
 # compile the server using the cgo
 go build -tags "oss nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
-export CGO_ENABLED=0
-go build -tags "oss nolimit" -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -tags "oss nolimit" -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
