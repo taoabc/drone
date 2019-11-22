1b866085f4c915aaa8f464624c01f99b4787f209
huhaitao <huhaitao@100tal.com>
2019-11-22 18:04:12 +0800

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
-go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+go build -ldflags -tags "oss nolimit" "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
 # compile the runners with gcc disabled
 export CGO_ENABLED=0
-go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
+go build -tags "oss nolimit" -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
+go build -tags "oss nolimit" -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
