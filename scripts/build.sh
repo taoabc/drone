f3a531e9196f8fcce40b2b6ab57463db170c6581
huhaitao <huhaitao@tal.com>
2020-10-19 11:23:20 +0800

    change build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,6 @@ REPO="github.com/drone/drone"
 go build -tags "oss nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
 # compile the runners with gcc disabled
-# export CGO_ENABLED=0
+export CGO_ENABLED=0 # 上一个步骤可能导致一个失败，使用这个语句来解决失败问题
 # go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
 # go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
